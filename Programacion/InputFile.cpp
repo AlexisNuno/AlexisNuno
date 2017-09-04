c57d532989676b3d0811338357599c4a8363c0de
Alexis Gonzalez <ajgn95@hotmail.com>
2017-09-04 17:15:40 -0500

    Tarea InputFile

---
+++ Programacion/InputFile.cpp
@@ -0,0 +1,31 @@
+#pragma once
+#include "InputFile.h"
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+bool InputFile::Read(std::string filename)
+{
+	std::fstream fs;
+	fs.open("Prueba,txt");
+
+	if (fs.is_open()) {
+		std::stringstream ss;
+		ss << fs.rdbuf();
+		_contents = ss.str();
+
+		return true;
+
+	}
+	else {
+		return false;
+
+	}
+}
+	std::string InputFile::GetContents()
+	{
+		return std::string(_contents);
+
+	}
+
+

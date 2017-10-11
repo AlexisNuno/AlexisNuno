9360235d79748dd6e0366d876d70384628467558
Alexis Gonzalez <ajgn95@gmail.com>
2017-10-11 17:12:01 -0500

    InputFile

--- Programacion/InputFile.cpp
+++ Programacion/InputFile.cpp
@@ -1,31 +1,33 @@
-#pragma once
 #include "InputFile.h"
-#include <iostream>
+
 #include <fstream>
+#include <iostream>
 #include <sstream>
 
 bool InputFile::Read(std::string filename)
 {
-	std::fstream fs;
-	fs.open("Prueba,txt");
-
-	if (fs.is_open()) {
-		std::stringstream ss;
-		ss << fs.rdbuf();
-		_contents = ss.str();
-
-		return true;
-
-	}
-	else {
-		return false;
-
-	}
-}
-	std::string InputFile::GetContents()
+	if (filename.empty())
 	{
-		return std::string(_contents);
-
+		std::cout << "No filename provided" << std::endl;
+		return false;
 	}
 
+	std::fstream inputFile(filename, std::fstream::in);
 
+	if (!inputFile.is_open())
+	{
+		std::cout << "Could not open file " << filename << std::endl;
+		return false;
+	}
+
+	std::stringstream ss;
+	ss << inputFile.rdbuf();
+	_contents = ss.str();
+
+	return true;
+}
+
+std::string InputFile::GetContents()
+{
+	return _contents;
+}

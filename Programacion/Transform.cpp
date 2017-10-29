6b08417bce9031133ae4109b9ef9ade372c1d0ec
Alexis Gonzalez <ajgn95@gmail.com>
2017-10-28 21:07:42 -0500

    Completo Examen Segundo Registro

--- Programacion/Transform.cpp
+++ Programacion/Transform.cpp
@@ -12,8 +12,6 @@ Transform::Transform()
 	_rotation = glm::quat(glm::radians(glm::vec3(0.0f, 0.0f, 0.0f)));
 	_position = glm::vec3(0, 0, 0);
 	_scale = glm::vec3(1.0, 1.0, 1.0);
-	
-
 }
 
 glm::mat4 Transform::GetModelMatrix() {
@@ -35,4 +33,30 @@ glm::vec3 Transform::GetScale() {
 	return glm::vec3(_scale);
 }
 
-void setPosition
+void Transform::setPosition(float x, float y, float z) {
+	_position = glm::vec3(x, y, z);
+	updateModelMatrix();
+}
+void Transform::setRotation(float x, float y, float z) {
+	_rotation = glm::vec3(glm::radians(glm::vec3(x, y, z)));
+	updateModelMatrix();
+}
+void Transform::setScale(float x, float y, float z) {
+	_scale = glm::vec3(x, y, z);
+	updateModelMatrix();
+}
+
+void Transform::updateModelMatrix() {
+
+	_modelMatrix = glm::mat4_cast(_rotation);
+
+	_modelMatrix[0] = _modelMatrix[0] * _scale[0];
+	_modelMatrix[1] = _modelMatrix[1] * _scale[1];
+	_modelMatrix[2] = _modelMatrix[2] * _scale[2];
+
+	_modelMatrix[3][0] = _position[0];
+	_modelMatrix[3][1] = _position[1];
+	_modelMatrix[3][2] = _position[2];
+
+}
+

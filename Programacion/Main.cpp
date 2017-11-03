76a0f37b37a9e18af3b5694fc03c0ffa9832c2ed
Alexis Gonzalez <ajgn95@gmail.com>
2017-11-03 12:02:39 -0600

    Proyecto Final

--- Programacion/Main.cpp
+++ Programacion/Main.cpp
@@ -1,46 +1,211 @@
-#include <iomanip>
-#include "Transform.h"
+#include <GL/glew.h>
+#include <GL/freeglut.h>
 #include <iostream>
+#include <glm/glm.hpp>
+#include <vector>
+#include "Camera.h"
+#include "Mesh.h"
+#include "ShaderProgram.h"
+#include "Transform.h"
+
+Camera _camera;
+Mesh _mesh;
+Transform _transform;
+Transform _t2;
+ShaderProgram _shaderProgram;
+
+// Función que va a inicializar toda la memoria del programa.
+void Initialize()
+{
+	// Vamos a crear una lista que va a almacenar las posiciones
+	// en 2 dimensiones de un triángulo.
+	// Esto es en CPU y RAM.
+	std::vector<glm::vec3> positions;
+	// Cara frontal
+	positions.push_back(glm::vec3(-3.0f, -3.0f, 3.0f));
+	positions.push_back(glm::vec3(3.0f, -3.0f, 3.0f));
+	positions.push_back(glm::vec3(3.0f, 3.0f, 3.0f));
+	positions.push_back(glm::vec3(-3.0f, 3.0f, 3.0f));
+	// Cara lateral derecha
+	positions.push_back(glm::vec3(3.0f, -3.0f, 3.0f));
+	positions.push_back(glm::vec3(3.0f, -3.0f, -3.0f));
+	positions.push_back(glm::vec3(3.0f, 3.0f, -3.0f));
+	positions.push_back(glm::vec3(3.0f, 3.0f, 3.0f));
+	//Cara Trasera
+	positions.push_back(glm::vec3(3.0f, -3.0f, -3.0f));
+	positions.push_back(glm::vec3(-3.0f, -3.0f, -3.0f));
+	positions.push_back(glm::vec3(-3.0f, 3.0f, -3.0f));
+	positions.push_back(glm::vec3(3.0f, 3.0f, -3.0f));
+	//Cara lateral izquierda
+	positions.push_back(glm::vec3(-3.0f, -3.0f, -3.0f));
+	positions.push_back(glm::vec3(-3.0f, -3.0f, 3.0f));
+	positions.push_back(glm::vec3(-3.0f, 3.0f, 3.0f));
+	positions.push_back(glm::vec3(-3.0f, 3.0f, -3.0f));
+	//Cara Superior
+	positions.push_back(glm::vec3(-3.0f, 3.0f, 3.0f));
+	positions.push_back(glm::vec3(3.0f, 3.0f, 3.0f));
+	positions.push_back(glm::vec3(3.0f, 3.0f, -3.0f));
+	positions.push_back(glm::vec3(-3.0f, 3.0f, -3.0f));
+
+	//Cara Inferior 
+	positions.push_back(glm::vec3(-3.0f, -3.0f, -3.0f));
+	positions.push_back(glm::vec3(3.0f, -3.0f, -3.0f));
+	positions.push_back(glm::vec3(3.0f, -3.0f, 3.0f));
+	positions.push_back(glm::vec3(-3.0f, -3.0f, 3.0f));
 
 
-Transform rotation;
-Transform translation;
-Transform scale;
-Transform combined;
+	// Vamos a crear una lista para almacenar colores RGB
+	// Esta lista está en CPU y RAM
+	std::vector<glm::vec3> colors;
+	// Cara frontal
+	colors.push_back(glm::vec3(1.0f, 0.0f, 0.0f));
+	colors.push_back(glm::vec3(1.0f, 0.0f, 0.0f));
+	colors.push_back(glm::vec3(1.0f, 0.0f, 0.0f));
+	colors.push_back(glm::vec3(1.0f, 0.0f, 0.0f));
+	// Cara lateral derecha
+	colors.push_back(glm::vec3(0.0f, 1.0f, 0.0f));
+	colors.push_back(glm::vec3(0.0f, 1.0f, 0.0f));
+	colors.push_back(glm::vec3(0.0f, 1.0f, 0.0f));
+	colors.push_back(glm::vec3(0.0f, 1.0f, 0.0f));
+	//Cara trasera
+	colors.push_back(glm::vec3(0.0f, 0.0f, 1.0f));
+	colors.push_back(glm::vec3(0.0f, 0.0f, 1.0f));
+	colors.push_back(glm::vec3(0.0f, 0.0f, 1.0f));
+	colors.push_back(glm::vec3(0.0f, 0.0f, 1.0f));
+	//Cara lateral izquierda
+	colors.push_back(glm::vec3(1.0f, 0.0f, 1.0f));
+	colors.push_back(glm::vec3(1.0f, 0.0f, 1.0f));
+	colors.push_back(glm::vec3(1.0f, 0.0f, 1.0f));
+	colors.push_back(glm::vec3(1.0f, 0.0f, 1.0f));
+	//Cara superior
+	colors.push_back(glm::vec3(1.0f, 1.0f, 1.0f));
+	colors.push_back(glm::vec3(1.0f, 1.0f, 1.0f));
+	colors.push_back(glm::vec3(1.0f, 1.0f, 1.0f));
+	colors.push_back(glm::vec3(1.0f, 1.0f, 1.0f));
+	//Cara inferior
+	colors.push_back(glm::vec3(0.5f, 0.5f, 1.0f));
+	colors.push_back(glm::vec3(0.5f, 0.5f, 1.0f));
+	colors.push_back(glm::vec3(0.4f, 0.5f, 1.0f));
+	colors.push_back(glm::vec3(0.5f, 0.5f, 1.0f));
 
 
 
+	std::vector<unsigned int> indices = { 0, 1, 2, 0, 2, 3, 4, 5, 6, 4, 6, 7,8,9,10,8,10,11,12,14,15,12,13,14,16,18,19,16,17,18,20,22,23,20,21,22 };
 
-void PrintTransform(Transform& t) {
-	glm::mat4 _modelMatrix = t.GetModelMatrix();
-	for (size_t i = 0; i < _modelMatrix.length(); i++) {
-		for (size_t j = 0; j < _modelMatrix.length(); j++) {
+	_mesh.CreateMesh(24);
+	_mesh.SetPositionAttribute(positions, GL_STATIC_DRAW, 0);
+	_mesh.SetColorAttribute(colors, GL_STATIC_DRAW, 1);
+	_mesh.SetIndices(indices, GL_STATIC_DRAW);
 
-			std::cout << std::setw(10) << std::left << _modelMatrix[j][i] << " ";
-		}
-		std::cout << std::endl;
-	}
-	std::cout << std::endl;
+	_shaderProgram.CreateProgram();
+	_shaderProgram.AttachShader("Default.vert", GL_VERTEX_SHADER);
+	_shaderProgram.AttachShader("Default.frag", GL_FRAGMENT_SHADER);
+	_shaderProgram.SetAttribute(0, "VertexPosition");
+	_shaderProgram.SetAttribute(1, "VertexColor");
+	_shaderProgram.LinkProgram();
+
+	_transform.SetPosition(-5.0f, 0.0f, -20.0f);
+	_t2.SetPosition(5.0f, 0.0f, -20.0f);
+
 }
 
+void MainLoop()
+{
+	// Borramos el buffer de color y profundidad siempre al inicio de un nuevo frame.
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-int main() {
-	rotation.setRotation(45, 90, 30);
-	PrintTransform(rotation);
+	_transform.Rotate(0.03f, 0.04f, -0.01f, true);
 
-	translation.setPosition(3, 5, -7);
-	PrintTransform(translation);
 
-	scale.setScale(2, 3, 4);
-	PrintTransform(scale);
+	_shaderProgram.Activate();
+	_shaderProgram.SetUniformMatrix("mvpMatrix", _camera.GetViewProjection() * _transform.GetModelMatrix());
+	_mesh.Draw(GL_TRIANGLES);
+	_shaderProgram.Deactivate();
 
-	combined.setRotation(45, 90, 30);
-	combined.setPosition(3, 5, -7);
-	combined.setScale(2, 3, 4);
+	_t2.Rotate(-0.010f, -0.04f, 0.0f, true);
 
-	PrintTransform(combined);
 
-	getchar();
+	_shaderProgram.Activate();
+	_shaderProgram.SetUniformMatrix("mvpMatrix", _camera.GetViewProjection() * _t2.GetModelMatrix());
+	_mesh.Draw(GL_TRIANGLES);
+	_shaderProgram.Deactivate();
+
+
+	// Intercambiar los buffers (el que se estaba rendereando con el que se estaba
+	// mostrando).
+	glutSwapBuffers();
+}
+
+void Idle()
+{
+	// Cuando OpenGL entra en modo de reposo 
+	// (para guardar bateria, por ejemplo)
+	// le decimos que vuelva a dibujar ->
+	// Vuelve a mandar a llamar GameLoop
+	glutPostRedisplay();
+}
+
+void ReshapeWindow(int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+int main(int argc, char* argv[])
+{
+	// Inicializamos freeglut. 
+	// Freeglut se encarga de generar y manejar ventanas.
+	// Utilizamos freeglut para crear una ventana donde vamos a dibujar.
+	glutInit(&argc, argv);
+	// Tenemos que informar que queremos trabajar con el pipeline
+	// programable (OpenGL moderno).
+	glutInitContextProfile(GLUT_CORE_PROFILE);
+	// Freeglut nos permite configurar eventos que ocurren en la ventana.
+	// Un evento que nos interesa es cuando alguien cierra la ventana.
+	// En este caso, simplemente dejamos de renderear y terminamos el programa.
+	glutSetOption(GLUT_ACTION_ON_WINDOW_CLOSE, GLUT_ACTION_GLUTMAINLOOP_RETURNS);
+	// Configuramos el framebuffer. En este caso estamos solicitando un buffer
+	// de color true color RGBA, un buffer de profundidad y un segundo buffer 
+	// para rendererar (evitamos parpadeos al renderear).
+	glutInitDisplayMode(GLUT_RGBA | GLUT_DEPTH | GLUT_DOUBLE);
+	// Solicitamos una ventana de 400x400 pixeles.
+	glutInitWindowSize(400, 400);
+	// Creamos y abrimos la ventana con un título personalizado.
+	glutCreateWindow("Hello World OpenGL");
+	// Asociar una función de render. Esta función se mandará a llamar para 
+	// dibujar un frame.
+	glutDisplayFunc(MainLoop);
+	// Asociamos una función para el cambio de resolución
+	// de la ventana. Freeglut la va a mandar a llamar
+	// cuando alguien cambie el tamaño de la venta.
+	glutReshapeFunc(ReshapeWindow);
+	// Asociamos la función que se mandará a llamar
+	// cuando OpenGL entre en modo de reposo.
+	glutIdleFunc(Idle);
+
+	// Inicializamos GLEW. Esta librería se encarga de obtener el API
+	// de OpenGL de nuestra tarjeta de video. SHAME ON YOU MICROSOFT.
+	glewInit();
+
+	// COnfigurar OpenGL. Este es el color por default del buffer de color
+	// en el framebuffer.
+	glClearColor(1.0f, 1.0f, 0.5f, 1.0f);
+	// Ademas de solicitar el buffer de profundidad, tenemos
+	// que decirle a OpenGL que lo queremos activo
+	glEnable(GL_DEPTH_TEST);
+	// Activamos el borrado de caras traseras.
+	// Ahora todos los triangulos que dibujemos deben estar en CCW
+	glEnable(GL_CULL_FACE);
+	// No dibujar las caras traseras de las geometrías.
+	glCullFace(GL_BACK);
+
+	std::cout << glGetString(GL_VERSION) << std::endl;
+
+	// Configurar la memoria que la aplicación va a necesitar.
+	Initialize();
+
+	// Iniciamos la aplicacion gráfica. El main se pausará en estea línea
+	// hasta que se cierre la ventana de OpenGL.
+	glutMainLoop();
+
 	return 0;
-
 }
